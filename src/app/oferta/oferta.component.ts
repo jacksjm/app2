639c12538956ef428ad0f39c0c6be9639ac0c2c9
Jackson Machado <jackson_machado_12@hotmail.com>
2017-12-01 00:21:31 -0200

    aula 124

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model'
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public oferta: Oferta
+
+  constructor(private route: ActivatedRoute, private ofertaService: OfertasService ) { }
 
   ngOnInit() {
+	  //Recuperação com Snapshot
+	  //this.route.snapshot.params.id
+	  //Recuperação com Subscribe
+	  //this.route.params.subscribe((parametro: any)=>{console.log(parametro)})
+	/*this.ofertaService.getOfertaId(this.route.snapshot.params.id)
+	.then(
+		( ofertas: Array<Oferta> ) => {
+				this.oferta = ofertas[0]
+				console.log(this.oferta)
+			}
+	)
+	.catch(
+		( param: any ) => { console.log( param ) }
+	)*/
   }
 
 }
