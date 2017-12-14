d8cd27f268b09daddd5ef71f2a0468a0c184dbcf
Jackson Machado <jackson.machado@ng.local>
2017-12-14 19:46:04 -0200

    Aula 151

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http'
+import { Http, Response } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from "./shared/oferta.model"
 import { URL_API } from "./app.api"
@@ -17,34 +17,34 @@ export class OfertasService {
 		//Efetua uma requisição HTTP e retorna um promisse Array<Oferta>
 		return this.http.get(`${URL_API}/ofertas?destaque=true`)
 			.toPromise()
-			.then( ( resposta: any ) => resposta.json() )
+			.then( ( resposta: Response ) => resposta.json() )
 	}
 	public getOfertasPorCategorias(categoria: string): Promise<Array<Oferta>> {
 		return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
 			.toPromise()
-			.then( ( resposta: any ) => resposta.json() )
+			.then( ( resposta: Response ) => resposta.json() )
 	}
 	public getOfertaPorId(id: number): Promise<Oferta>{
 		return this.http.get(`${URL_API}/ofertas?id=${id}`)
 		.toPromise()
-		.then( ( resposta: any ) => resposta.json().shift() )
+		.then( ( resposta: Response ) => resposta.json().shift() )
 	}
 
 	public getComoUsarOfertaPorId(id: number): Promise<string>{
 		return this.http.get(`${URL_API}/como-usar?id=${id}`)
 		.toPromise()
-		.then( ( resposta: any ) => resposta.json().shift().descricao )
+		.then( ( resposta: Response ) => resposta.json().shift().descricao )
 	}
 
 	public getOndeFicaOfertaPorId(id: number): Promise<string>{
 		return this.http.get(`${URL_API}/onde-fica?id=${id}`)
 		.toPromise()
-		.then( ( resposta: any ) => resposta.json().shift().descricao )
+		.then( ( resposta: Response ) => resposta.json().shift().descricao )
 	}
 
 	public pesquisaOfertas( termo: string): Observable<Array<Oferta>>{
 		return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
 			.retry(10)
-			.map( (resposta: any) => resposta.json() )
+			.map( (resposta: Response) => resposta.json() )
 	}
 }
