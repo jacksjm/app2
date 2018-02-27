d6220637959ddab068bd51fcbefb19f942ba0bc2
Jackson Machado <jackson.machado@ng.local>
2018-02-27 18:06:05 -0300

    Aula 186

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,7 +1,28 @@
-import { Pedido } from './shared/pedido.model'
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Pedido } from './shared/pedido.model';
+import { URL_API } from './app.api';
+import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retry';
+
+@Injectable()
 export class OrdemCompraService {
-	public efetivarCompra(pedido: Pedido): void{
-		console.log(pedido)
+
+	constructor(private http: Http) {}
+
+	public efetivarCompra(pedido: Pedido): Observable<any> {
+
+		let headers: Headers = new Headers()
+
+		headers.append('Content-type', 'application/json')
+
+		return this.http.post(
+			`${URL_API}/pedidos` ,
+			JSON.stringify(pedido),
+			new RequestOptions({ headers: headers })
+		)
+		.map((resposta: Response) => console.log( resposta.json() ) )
 	}
 }
