ffe79be0ef0fc3ae0e445a7e1ccfd1c510246da4
Jackson Machado <jackson.machado@ng.local>
2018-02-27 17:15:31 -0300

    Aula 181

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -18,6 +18,16 @@ export class OrdemCompraComponent implements OnInit {
   public complementoValido: boolean
   public formaPagamentoValido: boolean
 
+
+  //estados primitivos dos campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+
+  //controlar botão confirmar compra
+  public formEstado: string = 'disabled'
+
   constructor() { }
 
   ngOnInit() {
@@ -25,7 +35,7 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
 	  this.endereco = endereco
-	  console.log(this.endereco)
+	  //console.log(this.endereco)
 
 	  //se a string for maior que 3
 	  if (this.endereco.length > 3){
@@ -34,32 +44,51 @@ export class OrdemCompraComponent implements OnInit {
 		  this.enderecoValido = false
 	  }
 
+	  this.enderecoEstadoPrimitivo = false
+	  this.habilitaForm()
   }
   public atualizaNumero(numero: string): void {
 	this.numero = numero
-	console.log(this.numero)
+	//console.log(this.numero)
 	 //se a string for maior que 0
-	if (this.endereco.length > 0){
+	if (this.numero.length > 0){
 		this.numeroValido = true
 	}else{
 		this.numeroValido = false
 	}
+	//console.log(this.numeroValido)
+	this.numeroEstadoPrimitivo = false
+	this.habilitaForm()
   }
   public atualizaComplemento(complemento: string): void {
 	this.complemento = complemento
-	console.log(this.complemento)
+	//console.log(this.complemento)
 	if (this.complemento.length > 0 ){
 		this.complementoValido = true
-    }
+	}
+
+	this.complementoEstadoPrimitivo = false
+	this.habilitaForm()
   }
   public atualizaFormaPagamento(formaPagamento: string): void {
 	this.formaPagamento = formaPagamento
-	console.log(this.formaPagamento)
+	//console.log(this.formaPagamento)
 
 	if (this.formaPagamento.length > 0 ){
 		this.formaPagamentoValido = true
 	}else{
 		this.formaPagamentoValido = false
+	}
+
+	this.formaPagamentoEstadoPrimitivo = false
+	this.habilitaForm()
+  }
+
+  public habilitaForm(): void{
+	if(this.enderecoValido === true && this.numeroValido === true && this.formaPagamentoValido === true){
+		this.formEstado = ''
+	}else{
+		this.formEstado = 'disabled'
 	}
   }
 }
