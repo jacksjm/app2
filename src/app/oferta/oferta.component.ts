313c6ba70164f3600b58ae0e00e20f4e42377a40
Jackson Machado <jackson.machado@ng.local>
2018-02-28 16:56:39 -0300

    Aula 215

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'
 import { OfertasService } from '../ofertas.service';
-import { Oferta } from '../shared/oferta.model'
+import CarrinhoService from '../carrinho.service';
+
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [ OfertasService ]
+  providers: [ OfertasService, CarrinhoService ]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
 
   public oferta: Oferta
 
-  constructor(private route: ActivatedRoute, private ofertaService: OfertasService ) { }
+  constructor(
+	  private route: ActivatedRoute,
+	  private ofertaService: OfertasService,
+	  private carrinhoService: CarrinhoService
+	) { }
 
   ngOnInit() {
 	  //Recuperação com Snapshot
 	  //this.route.snapshot.params.id
 	  //Recuperação com Subscribe
 	  //this.route.params.subscribe((parametro: any)=>{console.log(parametro)})
+	console.log(this.carrinhoService.exibirItens())
 	this.route.params.subscribe((parametros: Params) => {
 		this.ofertaService.getOfertaPorId(parametros.id)
 		.then(
