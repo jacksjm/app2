13ad0f200f6e6ae0c657c2db77c031d6f52063b3
Jackson Machado <jackson.machado@ng.local>
2018-02-28 16:33:36 -0300

    Aula 210

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,6 +11,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra:number
   public formulario: FormGroup = new FormGroup({
 	  'endereco': new FormControl(null, [ Validators.required , Validators.minLength(3), Validators.maxLength(120) ]),
 	  'numero': new FormControl(null, [ Validators.required , Validators.minLength(1), Validators.maxLength(20) ]),
@@ -30,6 +31,17 @@ export class OrdemCompraComponent implements OnInit {
 		  this.formulario.get('numero').markAsTouched()
 		  this.formulario.get('complemento').markAsTouched()
 		  this.formulario.get('formaPagamento').markAsTouched()
+	  }else{
+		  let pedido = new Pedido(
+			  this.formulario.value.endereco,
+			  this.formulario.value.numero,
+			  this.formulario.value.complemento,
+			  this.formulario.value.formaPagamento)
+
+		  this.ordemCompraService.efetivarCompra(pedido)
+			.subscribe( (idPedido: number) => {
+				this.idPedidoCompra = idPedido
+			})
 	  }
   }
 }
