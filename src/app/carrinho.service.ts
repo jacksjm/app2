46ee7ad550576de24d94de1199954df18f5e9cde
Jackson Machado <jackson.machado@ng.local>
2018-02-28 16:45:47 -0300

    Aula 213

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,5 +1,7 @@
-class CarrinhoService {
+import { ItemCarrinho } from './shared/item-carrinho.model'
 
+class CarrinhoService {
+	public itens: Array<ItemCarrinho> = []
 }
 
 export default CarrinhoService
--- src/app/shared/pedido.model.ts
+++ src/app/shared/pedido.model.ts
@@ -3,6 +3,6 @@ export class Pedido {
 		public endereco: string,
 		public numero: string,
 		public complemento: string,
-		public formaPagamento: string,
+		public formaPagamento: string
 	) { }
 }
