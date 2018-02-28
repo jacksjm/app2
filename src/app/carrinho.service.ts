39bbf5eec60349b3d2bc0771ca447809a41ac531
Jackson Machado <jackson.machado@ng.local>
2018-02-28 16:51:29 -0300

    Aula 214

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -2,6 +2,10 @@ import { ItemCarrinho } from './shared/item-carrinho.model'
 
 class CarrinhoService {
 	public itens: Array<ItemCarrinho> = []
+
+	public exibirItens(): Array<ItemCarrinho> {
+		return this.itens
+	}
 }
 
 export default CarrinhoService
