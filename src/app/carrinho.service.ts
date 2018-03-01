90b6451ab6eba6e818b38b1fc1b179df267dd9bd
Jackson Machado <jackson.machado@ng.local>
2018-03-01 11:44:23 -0300

    Aula 222

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -37,6 +37,13 @@ class CarrinhoService {
 
 		return total
 	}
+
+	public adicionarQuantidade(itemCarrinho: ItemCarrinho): void{
+		let itemCarrinhoEncontrado = this.itens.find( (item: ItemCarrinho) => item.id === itemCarrinho.id )
+		if (itemCarrinhoEncontrado){
+			itemCarrinhoEncontrado.quantidade++
+		}
+	}
 }
 
 export { CarrinhoService }
